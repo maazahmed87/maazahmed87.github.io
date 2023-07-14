70ea90db5d445ca11e4585452741105c5a527b4e
Maaz Ahmed <elfilosof@protonmail.com>
2023-07-13 18:57:02 -0700

    restaurant done

--- js/script.js
+++ js/script.js
@@ -6,20 +6,6 @@ if (document.getElementById('my-work-link')) {
   });
 }
 
-const headerImage = document.querySelector('#portfolio-header-image-container');
-
-headerImage.style.transform = 'scale(0)';
-headerImage.style.opacity = 0;
-
-anime({
-  targets: headerImage,
-  opacity: 1,
-  scale: 1,
-  duration: 2000,
-  easing: 'easeOutElastic(1, .5)',
-  delay: 500
-});
-
 window.addEventListener('load', function () {
   const preloader = document.getElementById('preloader');
   preloader.style.display = 'none';
